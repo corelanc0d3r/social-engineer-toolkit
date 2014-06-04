2bae27e7cfd11ce40701c67bf0cf66877d38384d
corelanc0d3r <peter.ve@corelan.be>
2014-06-04 15:30:05 +0200

    Read templates from filesystem

--- src/html/templates/template.py
+++ src/html/templates/template.py
@@ -7,19 +7,33 @@ from src.core.setcore import *
 
 me = mod_name()
 dest = "src/html/"
+templateroot = os.path.join(dest,"templates")
 
 debug_msg(me,"entering src.html.templates.template'",1)
 
 #
 # used for pre-defined templates
 #
-print """
-  1. Java Required
-  2. Google
-  3. Facebook
-  4. Twitter
-  5. Yahoo
-"""
+
+templatedata = {}
+# data[0] = URL
+# data[1] = copy mode.  1 = one file, 2 = all files
+templatedata["java"] = ["",1]
+templatedata["google"] = ["http://www.google.com",1]
+templatedata["facebook"] = ["http://www.facebook.com",2]
+templatedata["twitter"] = ["http://www.twitter.com",1]
+templatedata["yahoo"] = ["http://mail.yahoo.com",1]
+
+# dynamically get template folder names
+templatelist = {}
+
+folderkey = 1
+for foldername in os.listdir(templateroot):
+    if os.path.isdir(os.path.join(templateroot,foldername)): 
+        templatelist[folderkey] = foldername.lower()
+        print "  %d. %s" % (folderkey, foldername.title())
+        folderkey += 1
+
 choice=raw_input(setprompt(["2"],"Select a template"))
 
 if choice == "exit":
@@ -31,36 +45,22 @@ filewrite=file(setdir + "/site.template", "w")
 # if nothing is selected
 if choice == "": choice = "1"
 
-# if java required
-if choice == "1":
+selectedtemplateid = int(choice)
+if selectedtemplateid in templatelist:
+    selectedtemplateloc = templatelist[selectedtemplateid]
     if os.path.isfile("src/html/index.template"): os.remove("src/html/index.template")
-    shutil.copyfile("src/html/templates/java/index.template", "src/html/index.template")
-    URL=""
-
-# if google
-if choice == "2":
-    if os.path.isfile("src/html/index.template"): os.remove("src/html/index.template")
-    shutil.copyfile("src/html/templates/google/index.template", "src/html/index.template")
-    URL="http://www.google.com"
-
-# if facebook
-if choice == "3":
-    if os.path.isfile("src/html/index.template"): os.remove("src/html/index.template")
-    for files in glob.glob('src/html/templates/facebook/*.*'): shutil.copy(files, "src/html/")
-    URL="http://www.facebook.com"
-
-# if twitter
-if choice == "5":
-    if os.path.isfile("src/html/index.template"): 
-      os.remove("src/html/index.template")
-    shutil.copyfile("src/html/templates/twitter/index.template", "src/html/index.template")
-    URL="http://www.twitter.com"
-
-# if yahoo
-if choice =="6":
-    if os.path.isfile("src/html/index.template"): os.remove("src/html/index.template")
-    shutil.copyfile("src/html/templates/yahoo/index.template", "src/html/index.template")
-    URL="http://mail.yahoo.com"
+    if selectedtemplateloc in templatedata:
+        URL = templatedata[selectedtemplateloc][0]
+        copymode = templatedata[selectedtemplateloc][1]
+    else:
+        # no prototype, default to blank URL and single file copy mode
+        URL = ""
+        copymode = 1
+    if copymode == 1:
+        shutil.copyfile("src/html/templates/%s/index.template" % selectedtemplateloc, "src/html/index.template")
+    elif copymode == 2:
+        for files in glob.glob('src/html/templates/%s/*.*' % selectedtemplateloc): shutil.copy(files, "src/html/")
+    
 
 if not os.path.isdir(setdir + "/web_clone"):
     os.makedirs(setdir + "/web_clone/")
